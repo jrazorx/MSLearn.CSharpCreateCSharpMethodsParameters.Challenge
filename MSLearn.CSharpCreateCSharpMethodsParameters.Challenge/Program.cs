920fb0e309a87086ec3a5648e7d6a838974ddd63
jra <julien.gault.50@gmail.com>
2024-03-15 12:06:04 +0100

    skeleton method DisplayEmployeeEmail for displaying emails of employees + calls of the method

--- MSLearn.CSharpCreateCSharpMethodsParameters.Challenge/Program.cs
+++ MSLearn.CSharpCreateCSharpMethodsParameters.Challenge/Program.cs
@@ -16,9 +16,16 @@ string externalDomain = "hayworth.com";
 for (int i = 0; i < corporate.GetLength(0); i++)
 {
     // display internal email addresses
+    DisplayEmployeeEmail(corporate[i, 0], corporate[i, 1]);
 }
 
 for (int i = 0; i < external.GetLength(0); i++)
 {
     // display external email addresses
+    DisplayEmployeeEmail(external[i, 0], external[i, 1], externalDomain);
+}
+
+void DisplayEmployeeEmail (string employeeFirstName, string employeeLastName, string domain = "contoso.com")
+{
+    
 }
