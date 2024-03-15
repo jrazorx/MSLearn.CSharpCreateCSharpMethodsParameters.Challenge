f08e8231d82cb216bd82eb8dce1432557cb3cfab
jra <julien.gault.50@gmail.com>
2024-03-15 11:59:28 +0100

    Add project files.

---
+++ MSLearn.CSharpCreateCSharpMethodsParameters.Challenge/Program.cs
@@ -0,0 +1,24 @@
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    // display internal email addresses
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    // display external email addresses
+}
